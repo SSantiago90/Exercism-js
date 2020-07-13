84dc1c3470375fe3b474616742f722fc95fbbfb8
Santiago Salkin <salkin.santiago@gmail.com>
2020-07-13 16:21:38 -0300

    submiting leap exercise

--- leap/leap.js
+++ leap/leap.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isLeap = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isLeap = (year) => {
+  if ( (year % 4 === 0) && ( (year % 100 !== 0 ) || (year % 400 === 0) ))
+    return true; 
+  else return false;
 };
